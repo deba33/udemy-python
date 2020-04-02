1c08f56693faa83a911de589eb612ed9ee5ae477
deba33 <deba.dbs.bkt@gmail.com>
2020-04-02 10:10:09 +0530

    Sets and Booleans in Python

---
+++ a_Python_Object_and_Data_Structure_Basics/11_sets.py
@@ -0,0 +1,18 @@
+# Sets are unordered collections of unique elements.
+
+myset = set()
+myset.add(1)
+
+print(myset)
+
+myset.add(2)
+
+print(myset)
+
+myset.add(2)
+
+print(myset)
+
+mylist = [1, 1, 2, 2, 3, 3, 3]
+
+print(set(mylist))
---
+++ a_Python_Object_and_Data_Structure_Basics/12_booleans.py
@@ -0,0 +1,6 @@
+# Booleans are operators that allow you to convey True or False statements.
+
+print(type(True))
+print(type(False))
+
+print(1 > 2)
