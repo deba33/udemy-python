3cf2007127b56ff84925e62e0474b96f73bbf3ff
deba33 <deba.dbs.bkt@gmail.com>
2020-04-14 22:00:35 +0530

    Map function in python

---
+++ d_Methods_and_Functions/04_map_function.py
@@ -0,0 +1,25 @@
+# map function
+
+
+def square(num):
+    return num**2
+
+
+my_nums = [1, 2, 3, 4, 5]
+
+for item in map(square, my_nums):
+    print(item)
+
+print(list(map(square, my_nums)))
+
+
+def splicer(mystring):
+    if len(mystring) % 2 == 0:
+        return "EVEN"
+    else:
+        return mystring[0]
+
+
+names = ["Andy", "Eve", "Sally"]
+
+print(list(map(splicer, names)))
