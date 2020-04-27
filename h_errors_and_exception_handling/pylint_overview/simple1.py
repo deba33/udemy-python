4d979ca2b788bfd5da1f48a86f7bc6a510b16166
deba33 <deba.dbs.bkt@gmail.com>
2020-04-27 10:23:40 +0530

    pylint overview

---
+++ h_errors_and_exception_handling/pylint_overview/simple1.py
@@ -0,0 +1,12 @@
+a = 1
+b = 2
+print(a)
+print(B)
+
+
+'''check errors with pylint
+1. install pylint
+    In command prompt or terminal run 'pip install pylint'
+2. cd LocationOfYourPythonScript 
+3. In command prompt or terminal run 'pylint script.py'
+'''
